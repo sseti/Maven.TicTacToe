9ba0751c0a8302ef168b37deb04bb857ad8b853f
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro.local>
2020-02-14 12:39:11 -0500

    TicTacToe Sandy Setiawan

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,74 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character [] [] puzzle;
     public Board(Character[][] matrix) {
+        this.puzzle = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+    for (int i = 0; i < puzzle.length; i++) {
+        if (puzzle[i][0] == 'X' && puzzle[i][1] == 'X' && puzzle[i][2] == 'X') {
+            return true;
+        }
+        if (puzzle[0][i] == 'X' && puzzle[1][i] == 'X' && puzzle[2][i] == 'X') {
+            return true;
+        }
+
     }
+        if (puzzle[0][0] == 'X' && puzzle[1][1] == 'X' && puzzle[2][2] == 'X') {
+        return true;
+    }
+        if (puzzle[0][2] == 'X' && puzzle[1][1] == 'X' && puzzle[2][0] == 'X') {
+        return true;
+    }
+        return false;
+}
 
     public Boolean isInFavorOfO() {
-        return null;
+
+    for (int i = 0; i < puzzle.length; i++) {
+        if (puzzle[i][0] == 'O' && puzzle[i][1] == 'O' && puzzle[i][2] == 'O') {
+            return true;
+        }
+        if (puzzle[0][i] == 'O' && puzzle[1][i] == 'O' && puzzle[2][i] == 'O') {
+            return true;
+        }
+
     }
+        if (puzzle[0][0] == 'O' && puzzle[1][1] == 'O' && puzzle[2][2] == 'O') {
+        return true;
+    }
+        if (puzzle[0][2] == 'O' && puzzle[1][1] == 'O' && puzzle[2][0] == 'O') {
+        return true;
+    }
+        return false;
+}
 
     public Boolean isTie() {
-        return null;
+
+       for (int i = 0; i < puzzle.length; i++) {
+        for (int j = 0; j < puzzle.length; j++) {
+            if (puzzle[i][j] == ' ') {
+                return false;
+            }
+        }
     }
+        return true;
+}
 
     public String getWinner() {
-        return null;
+
+
+        if (isInFavorOfO()) {
+            return "O";
+        }
+        if (isInFavorOfX()){
+            return "X";
+        }
+
+        return "";
     }
 
 }
